6b5f8d41eec73285f3b7e8c2b485f90826e3ffc2
Alekszane <alekszane@gmail.com>
2020-12-15 21:26:36 +0300

    Add Rectangle Tests
    Signed-off-by: Alekszane <alekszane@gmail.com>

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -8,7 +8,7 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
+        double result1 = SqArea.square(24, 5);
         System.out.println(" p = 6, k = 1, s = 2, real = " + result1);
     }
 }
