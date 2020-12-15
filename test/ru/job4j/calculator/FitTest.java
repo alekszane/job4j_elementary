dbcdbb0af61bda33e89d8e108baa697ad0beeee8
Alekszane <alekszane@gmail.com>
2020-12-15 20:47:09 +0300

    "Ideal weight" test
    Signed-off-by: Alekszane <alekszane@gmail.com>

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FitTest {
+    @Test
+    public void whenMan165Then74() {
+        short in = 165;
+        double expected = 74.75;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman165Then74() {
+        short in = 165;
+        double expected = 74.75;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
