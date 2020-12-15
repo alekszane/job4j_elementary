1b2fb379be702658396f7e17002d8209df0dd375
Alekszane <alekszane@gmail.com>
2020-12-15 20:34:40 +0300

    Correct Point
    Signed-off-by: Alekszane <alekszane@gmail.com>

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class Point {
 
     public static double distance(int x1, int y1, int x2, int y2) {
-        double powX = Math.pow(x1 + x2, 2);
-        double powY = Math.pow(y1 + y2, 2);
+        double powX = Math.pow(x1 - x2, 2);
+        double powY = Math.pow(y1 - y2, 2);
         return Math.sqrt(powX + powY);
     }
 
