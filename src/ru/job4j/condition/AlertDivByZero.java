0babd05e288b47aa25641d7f88e8c6c9900c52a6
Alekszane <alekszane@gmail.com>
2020-12-19 19:35:23 +0300

    PossibleDiv
    Signed-off-by: Alekszane <alekszane@gmail.com>

---
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+        possibleDiv(-9);
+    }
+
+    public static void possibleDiv(int number) {
+        if (number < 0) {
+            System.out.println("This is negative number");
+        } else if (number == 0) {
+            System.out.println("This is number = 0");
+        } else {
+            System.out.println("This is positive number");
+        }
+    }
+}
