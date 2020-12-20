5bfe034f9643a8b269a5b4be7a19b565daa2ba42
Alekszane <alekszane@gmail.com>
2020-12-20 23:32:24 +0300

    Math.abs [#421779]
    Signed-off-by: Alekszane <alekszane@gmail.com>

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    /** @noinspection checkstyle:ParenPad*/
+
     public static boolean exist(double ab, double bc, double ac) {
         if (ab + ac > bc && ac + bc > ab && ab + bc > ac) {
             return true;
