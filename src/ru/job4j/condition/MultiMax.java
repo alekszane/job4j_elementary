be281cc9798a2ccd272bd04550749e6212f114c2
Alekszane <alekszane@gmail.com>
2020-12-21 23:02:34 +0300

    Debugging a program in IDEA [#421766]
    Signed-off-by: Alekszane <alekszane@gmail.com>

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -1,24 +1,14 @@
 package ru.job4j.condition;
 
 public class MultiMax {
-    public static  int max(int first, int second, int third) {
-        int max = 0;
-
-        if (first > second) {
-            max = first;
-            if (first > third) {
-                max = third;
-            } else {
-                max = first;
-            }
-        } else {
-           max = second;
-            if (second > third) {
-                max = second;
-            } else {
-                max = third;
-            }
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if (first <= second && second >= third) {
+            result = second;
         }
-        return max;
+        if (second <= third && first <= third) {
+            result = third;
+        }
+        return result;
     }
 }
--- test/ru/job4j/condition/MultiMaxTest.java
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MultiMaxTest {
@@ -10,5 +10,47 @@ public class MultiMaxTest {
     public void whenSecondMax() {
         int result = MultiMax.max(1, 4, 2);
         assertThat(result, is(4));
+    }
+
+    @Test
+    public void firstMax() {
+        int rsl = MultiMax.max(10, 5, 1);
+        assertThat(rsl, is(10));
+    }
+
+    @Test
+    public void secondMax() {
+        int rsl = MultiMax.max(10, 50, 1);
+        assertThat(rsl, is(50));
+    }
+
+    @Test
+    public void thirdMax() {
+        int rsl = MultiMax.max(1, 5, 100);
+        assertThat(rsl, is(100));
+    }
+
+    @Test
+    public void allEq() {
+        int rsl = MultiMax.max(1, 1, 1);
+        assertThat(rsl, is(1));
+    }
+
+    @Test
+    public void firstEqSecond() {
+        int rsl = MultiMax.max(10, 10, 1);
+        assertThat(rsl, is(10));
+    }
+
+    @Test
+    public void firstEqThird() {
+        int rsl = MultiMax.max(100, 1, 100);
+        assertThat(rsl, is(100));
+    }
+
+    @Test
+    public void secondEqThird() {
+        int rsl = MultiMax.max(1, 100, 100);
+        assertThat(rsl, is(100));
     }
 }
