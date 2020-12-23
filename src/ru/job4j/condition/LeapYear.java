b2294a39456b7abe6f6fcb9e503a53ffc9b465e6
Alekszane <alekszane@gmail.com>
2020-12-23 23:21:41 +0300

    LeapYear
    Signed-off-by: Alekszane <alekszane@gmail.com>

---
+++ src/ru/job4j/condition/LeapYear.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class LeapYear {
+    public static boolean checkYear(int year) {
+        boolean rls = false;
+        if (year % 400 == 0 || year % 4 == 0 && !(year % 100 == 0)) {
+            rls = true;
+        }
+        return rls;
+    }
+
+    public static void main(String[] args) {
+        boolean b = checkYear(2019);
+        System.out.println(b);
+        boolean c = checkYear(1800);
+        System.out.println(c);
+        boolean d = checkYear(2020);
+        System.out.println(d);
+        boolean e = checkYear(2000);
+        System.out.println(e);
+    }
+}
