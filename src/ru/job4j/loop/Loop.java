84e05b2332d370ccb50b738d1ab5ae8ca2569329
Alekszane <alekszane@gmail.com>
2020-12-26 23:43:00 +0300

    Generating constructs in IDEA
    Signed-off-by: Alekszane <alekszane@gmail.com>

---
+++ src/ru/job4j/loop/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("It is the very simple!!!");
+        }
+    }
+}
