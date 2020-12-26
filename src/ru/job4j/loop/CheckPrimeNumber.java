efe7e0be6a1c3ecf78b0e6347fdc37b85b1dbef8
Alekszane <alekszane@gmail.com>
2020-12-26 20:16:50 +0300

    Fix Fitness and CheckPrime
    Signed-off-by: Alekszane <alekszane@gmail.com>

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,10 +2,8 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int num) {
-        boolean prime = true;
-        if (num == 1) {
-            prime = false;
-        }
+        boolean prime = num > 1;
+
         for (int i = 2; i < num - 1; i++) {
             if (num % i == 0) {
                 prime = false;
