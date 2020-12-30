554f6992843533748ed11878ac677a5094e0777d
Alekszane <alekszane@gmail.com>
2020-12-31 00:14:07 +0300

    Swap array elements
    Signed-off-by: Alekszane <alekszane@gmail.com>

---
+++ test/ru/job4j/array/SwitchArrayTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SwitchArrayTest {
+
+    @Test
+    public void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int[] expect = {4, 2, 3, 1};
+        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
+    public void whenSwap0to4() {
+        int[] input = {1, 10, 2, 15, 5};
+        int[] expect = {5, 10, 2, 15, 1};
+        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
+    public void whenSwapBorder0to3() {
+        int[] input = {1, 2, 3, 4};
+        int[] expect = {4, 2, 3, 1};
+        int[] rsl = SwitchArray.swapBorder(input);
+        assertThat(rsl, is(expect));
+    }
+}
