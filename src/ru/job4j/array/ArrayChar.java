f5560ae3d590628d58e10a3b8433c2265f979c55
Alekszane <alekszane@gmail.com>
2021-01-02 06:52:14 +0300

    Array Char
    Signed-off-by: Alekszane <alekszane@gmail.com>

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] words, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            result = (words[i] == pref[i]) ? result : false;
+        }
+        return  result;
+    }
+}
