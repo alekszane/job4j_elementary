f47a5d431e1edb349bd13fdb48cf5fd0d50cc972
Alekszane <alekszane@gmail.com>
2021-01-03 07:06:34 +0300

    Search min in range
    Signed-off-by: Alekszane <alekszane@gmail.com>

---
+++ src/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class MinDiapason {
+    public static int findMin(int[] arr, int start, int finish) {
+        int min = arr[start];
+        for (int i = start; i <= finish; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
