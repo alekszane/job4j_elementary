e7bc44b848016f19d8ae78c66d6f54b5368a9cdf
All <all-13132yandex.ru>
2021-01-08 05:12:55 +0300

    fix Matrix Check

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,7 +4,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean rls = true;
         for (int i = 0; i < board[row].length; i++) {
-            if (board[row][i] != (int)'X') {
+            if (board[row][i] != 'X') {
                 rls = false;
                 break;
             }
@@ -15,7 +15,7 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board[column].length; i++) {
-            if (board[i][column] != (int)'X') {
+            if (board[i][column] != 'X') {
                 result = false;
                 break;
             }
