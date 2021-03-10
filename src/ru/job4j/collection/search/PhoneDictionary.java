971f23ec8dd6319a7b50d19b1a40c11ac45cafc8
Alekszane <alekszane@gmail.com>
2021-03-10 22:20:55 +0300

    Fix Phone Dictionary
    Signed-off-by: Alekszane <alekszane@gmail.com>

--- src/ru/job4j/collection/search/PhoneDictionary.java
+++ src/ru/job4j/collection/search/PhoneDictionary.java
@@ -14,10 +14,13 @@ public class PhoneDictionary {
      * @param key Key of search.
      * @return List persons
      */
+
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person: persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key) || person.getPhone().contains(key) || person.getAddress().contains(key)) {
+            if (person.getName().contains(key) || person.getSurname().contains(key) ||
+                    person.getPhone().contains(key) ||
+                    person.getAddress().contains(key)) {
                 result.add(person);
             }
         }
